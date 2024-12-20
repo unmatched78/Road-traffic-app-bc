2ed9e496ea63171e8a397cd61b181baea33c12e5
Viella <168560797+unmatched78@users.noreply.github.com>
2024-12-20 03:21:16 +0200

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -96,11 +96,21 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+DATABASES = {  
+    'default': {  
+        'ENGINE': 'django.db.backends.postgresql',  
+        'NAME': 'thierry',  
+        'USER': 'thierry12',  
+        'PASSWORD': 'xfvjvjHqIbVB3eRBY07OKxykKDG4vaLQ',  
+        'HOST': 'dpg-ct0bem1u0jms73dofvo0-a.singapore-postgres.render.com',  # just the host  
+        'PORT': '5432',  # ensure this line has correct indentation  
+    }  
 }
 
 
