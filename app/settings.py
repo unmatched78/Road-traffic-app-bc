4f7bcc1483fe888537bc56781c4d6083f40c86bb
Viella <168560797+unmatched78@users.noreply.github.com>
2024-12-30 14:02:36 +0200

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -159,11 +159,11 @@ USE_L10N = True #This setting, when set to True, enables localization of date an
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-#STATIC_URL = 'static/'
+STATIC_URL = 'static/'
 
-#STATICFILES_DIRS = [
-#    os.path.join(BASE_DIR, "static"),
-#]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 #MEDIA_URL = '/media/'
 #MEDIA_ROOT = os.path.join(BASE_DIR, "media")
