1290f2062f1cf4268f1dfe16c2c7d5c0e0b7830a
Viella <168560797+unmatched78@users.noreply.github.com>
2024-12-30 12:58:47 +0200

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -98,7 +98,7 @@ import cloudinary.uploader
 import cloudinary.api	
 cloudinary.config( 
   	cloud_name = "your_cloud_name",
-  	api_key = "your_api_key",
+  	api_key = "879745363269744",
   	api_secret = "your_api_secret"
 )
 
